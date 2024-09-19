8fcbe1baae18310dce31223692d8f4127818c85d
Hans Liu <hansliu40815@gmail.com>
2024-09-19 10:27:46 +0800

    modify: modify train command in execute.sh

--- UDeepSC/execute.sh
+++ UDeepSC/execute.sh
@@ -1,15 +1,15 @@
-CUDA_VISIBLE_DEVICES=1  python3  udeepsc_main.py \
+CUDA_VISIBLE_DEVICES=3  python3  udeepsc_main.py \
     --model  UDeepSC_new_model  \
-    --output_dir ckpt_record_12dB_single   \
+    --output_dir output   \
     --batch_size 16 \
     --input_size 32 \
-    --lr  6e-6 \
+    --lr  3e-5 \
     --epochs 500  \
     --opt_betas 0.95 0.99  \
     --save_freq 2   \
-    --ta_perform textr \
-    --resume ckpt_record_12dB_single/ckpt_msa/checkpoint-203.pth\
-    --eval
+    --ta_perform imgr \
+    # --resume ckpt_record_12dB_single/ckpt_msa/checkpoint-203.pth\
+    # --eval
    
   
 
