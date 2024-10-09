e71e030f3cd3f3c31202751a37a4a6800aed8553
Hans Liu <hansliu40815@gmail.com>
2024-10-09 09:31:08 +0800

    Modify deprecated torch._six to torch

--- UDeepSC/utils.py
+++ UDeepSC/utils.py
@@ -11,7 +11,7 @@ import numpy as np
 import torch.distributed as dist
 
 from pathlib import Path
-from torch._six import inf
+from torch import inf
 import torch.nn.functional as F
 from timm.utils import get_state_dict
 from timm.models import create_model
@@ -430,23 +430,23 @@ def create_ds_config(args):
 
         writer.write(json.dumps(ds_config, indent=2))
 
-def batch_index_select(x, idx):
-    if len(x.size()) == 3:
-        B, N, C = x.size()
-        N_new = idx.size(1)
-        offset = torch.arange(B, dtype=torch.long, device=x.device).view(B, 1) * N
-        idx = idx + offset
-        out = x.reshape(B*N, C)[idx.reshape(-1)].reshape(B, N_new, C)
-        return out
-    elif len(x.size()) == 2:
-        B, N = x.size()
-        N_new = idx.size(1)
-        offset = torch.arange(B, dtype=torch.long, device=x.device).view(B, 1) * N
-        idx = idx + offset
-        out = x.reshape(B*N)[idx.reshape(-1)].reshape(B, N_new)
-        return out
-    else:
-        raise NotImplementedError
+# def batch_index_select(x, idx):
+#     if len(x.size()) == 3:
+#         B, N, C = x.size()
+#         N_new = idx.size(1)
+#         offset = torch.arange(B, dtype=torch.long, device=x.device).view(B, 1) * N
+#         idx = idx + offset
+#         out = x.reshape(B*N, C)[idx.reshape(-1)].reshape(B, N_new, C)
+#         return out
+#     elif len(x.size()) == 2:
+#         B, N = x.size()
+#         N_new = idx.size(1)
+#         offset = torch.arange(B, dtype=torch.long, device=x.device).view(B, 1) * N
+#         idx = idx + offset
+#         out = x.reshape(B*N)[idx.reshape(-1)].reshape(B, N_new)
+#         return out
+#     else:
+#         raise NotImplementedError
 
 
 def psnr(img1, img2):
