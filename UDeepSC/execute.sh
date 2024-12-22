da80b2787900ade040a8ebb37650181645339674
Hansliu <hansliu40815@gmail.com>
2024-12-22 17:43:20 +0800

    Modify: modify parameter for running textr

--- UDeepSC/execute.sh
+++ UDeepSC/execute.sh
@@ -2,10 +2,10 @@
 python3  udeepsc_main.py \
     --model  UDeepSC_new_model  \
     --output_dir output   \
-    --batch_size 32 \
+    --batch_size 50 \
     --input_size 32 \
-    --lr  1e-5 \
-    --epochs 150  \
+    --lr  3e-5 \
+    --epochs 200  \
     --opt_betas 0.95 0.99  \
     --save_freq 50   \
     --ta_perform textr \
